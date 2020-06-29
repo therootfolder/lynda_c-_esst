fe3709c4999640c02bc5650d1fcf2d457a037a5f
lynda <lynda@gmail.com>
2020-06-29 15:31:34 +0530

    relational comparison operators

--- Chap04/working.cpp
+++ Chap04/working.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main()
 {
     puts("Hello, World!");
+
+    int x = 5;
+    int y = 42;
+    if(x ==y){
+       puts("true");
+    }else puts("false");
+                
+
     return 0;
 }
