429577f37024fb944f8c15e7b69ca3b150cdba08
lynda <lynda@gmail.com>
2020-06-29 21:25:43 +0530

    operators

--- Chap04/typeid.cpp
+++ Chap04/typeid.cpp
@@ -2,17 +2,28 @@
 // version of 2018-10-28
 #include <cstdio>
 #include <typeinfo>
+#include<string>
 using namespace std;
 
 struct A { int x; };
 struct B { int x; };
+/* even though they are similar they are
+different struct hence different user
+defined data types. they will have 
+different type ids or typeinfo objects
+ */
 
 A a1;
 B b1;
 
 int main() {
     if(typeid(a1) == typeid(A)) {
-        puts("same");
+        printf("same %s %s %s",typeid(A).name(), typeid(b1).name(),typeid( const int *).name());
+        //const char * -has type id-> PKc
+        // const double * has PKd
+        // char has c
+        // const * has PKi which is same for const int * 
+        printf(" %s\n",typeid(string).name());//NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE this gets printed out here
     } else {
         puts("different");
     }
--- Chap04/working.cpp
+++ Chap04/working.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 using namespace std;
 
+void func(int x);// you can have non 
+// matching parameter names while 
+// declaring vs  defining a function
 int main()
 {
     puts("Hello, World!");
@@ -12,7 +15,29 @@ bool b = false;
         puts("true");
     }else puts("false");
 
+
+func(100);
    
+/* int x = 5;
+long long int y ;
+y = x;
+x = (long int) y;
+printf(" x is %d \n",x);
+printf(" y is %lld \n",y); */
+
+int x= 5;
+size_t y = sizeof x;
+printf("size of x is %zd\n",y);
+
+
+
+
+
 
     return 0;
+}
+void func(int y){
+    int z = y;
+    ++z;
+    printf("%d\n",z);
 }
