353eb9a8d0dcdccb9c84fa64337fe01b7f61029d
lynda <lynda@gmail.com>
2020-06-29 14:41:42 +0530

    commit

--- Chap02/working.cpp
+++ Chap02/working.cpp
@@ -1,13 +1,12 @@
 // working.cpp by Bill Weinman <http://bw.org/>
 #include <cstdio>
 using namespace std;
-
 int main()
 {   //expression may be a part of the statement
     puts("Hello, World!");
 
     int x = 42;
     int y = 7;
- int z = 150;
+
     return 0;
 }
